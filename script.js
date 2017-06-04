1ea8e4b8d7f42eb605b8dab2868be4fb5ea989cb
Anna <a.chaikalemberg@gmail.com>
2017-06-05 00:08:45 +0300

    Adds 5th tasks solution

---
+++ script.js
@@ -0,0 +1,54 @@
+var boxes = document.querySelectorAll('.example1 div');
+
+for (var i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", capture, true);
+    boxes[i].addEventListener("click", bubble, false);
+}
+
+function capture() {
+    alert(this.classList);
+}
+
+function bubble() {
+    alert(this.classList);
+}
+
+
+function onForm() {
+    alert("stopPropagation зупиняє вспливання. Це повідомлення не з'являється, коли клікається по самому input");
+}
+
+function clickOnInput(event) {
+    alert('Функція виконується безпосередньо з input');
+    event.stopPropagation();
+}
+
+function clickOnLabel(event) {
+    alert('Функція виконується безпосередньо з label');
+    event.stopPropagation();
+    event.preventDefault();
+}
+
+function clickOnCheckbox () {
+alert('На клік на чекбоксі навішені два слухачі подій');
+event.stopImmediatePropagation();
+}
+
+function clickCheckbox() {
+    var message = this.getAttribute('data-lang');
+    alert(message);
+}
+
+document.querySelector('.button-container').addEventListener('click', onForm, false);
+
+document.querySelectorAll('[type=radio]').forEach(function(item) {
+    item.addEventListener('click', clickOnInput, false);});
+
+document.querySelectorAll('.button-container label').forEach(function(item) {
+    item.addEventListener('click', clickOnLabel, false);});
+
+document.querySelectorAll('[type=checkbox]').forEach(function(item) {
+    item.addEventListener('click', clickOnCheckbox, false);});
+
+document.querySelectorAll('[type=checkbox]').forEach(function(item) {
+    item.addEventListener('click', clickCheckbox, false);});
